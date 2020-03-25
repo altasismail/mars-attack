79a0fac587a21f5418a639dbfde173c2a7ff22fb
Neslihan44 <neslihantosunsg@gmail.com>
2020-03-25 13:21:51 +0100

    fotolar

--- php/view/section-home.php
+++ php/view/section-home.php
@@ -1,1 +1,6 @@
 <h1 class="comingSoon">COMING SOON...</h1>
+<section class="Fotolar"></section>
+<img src="assets/img/foto1.png" alt="1.foto">
+<img src="assets/img/foto2.jpg" alt="2.foto">
+<img src="assets/img/foto3.jpg" alt="3.foto">
+<img src="assets/img/foto4.jpg" alt="4.foto">
