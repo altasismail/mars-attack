5b773c9a29ae34e17cc77917c237194bf8046dd9
Neslihan44 <neslihantosunsg@gmail.com>
2020-04-06 15:20:40 +0200

    neslihan

--- php/view/section-blog.php
+++ php/view/section-blog.php
@@ -3,21 +3,26 @@
 <section class="blogPost">
     <div class="neslihan-blogResimleri">
         <img src="assets/img/blogPhotos/marsian.jpg" alt="">
+        <h2>Marsian a dit que Salut à tout l'univers</h2>
         <img src="assets/img/foto6.jpg" alt="">
+        <h2>Kahless a dit que Salut à tout l'univers</h2>
         <img src="assets/img/foto7.jpg" alt="">
+        <h2>Mercurien a dit que Salut à tout l'univers</h2>
         <img src="assets/img/foto8.jpg" alt="">
+        <h2>Terrien a dit que Salut à tout l'univers</h2>
 
     </div>
     <div class="blogTextBox">
-        <div class="blogTitle">
-            <h2>Marsian a dit que Salut à tout le monde</h2>
+        <div class="neslihan-blogTitle">
+    
+            
         </div>
         <div class="blogText">
-            <p>
+            <!-- <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi hic ab doloribus corrupti, 
                 asperiores iste reiciendis possimus rerum dignissimos earum repudiandae consequuntur provident voluptatem 
                 blanditiis deserunt natus, quia, veniam ducimus?
-            </p>
+            </p> -->
         </div>
     </div>
 </section>
