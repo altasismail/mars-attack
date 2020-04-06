b46dbcd21e261bedd5f5d145f01edf9901e5621f
merve16 <nmervekcygt@gmail.com>
2020-04-06 14:51:39 +0200

    merve

--- php/view/header.php
+++ php/view/header.php
@@ -4,16 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="assets/css/style.css">
-=======
-    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
->>>>>>> 1ef43c6ecff0eb793dfbe1fdb4d48937aa9d5bf1
     <link rel="stylesheet" href="assets/css/altas.css">
     <link rel="stylesheet" href="assets/css/aydin.css">
     <link rel="stylesheet" href="assets/css/merve.css">
     <link rel="stylesheet" href="assets/css/neslihan.css">
-<<<<<<< HEAD
 
 
 </head>
@@ -35,34 +30,6 @@
       </nav>
     </section> 
   
-=======
-
-</head>
-<body>
-  <header>
-    <section class="starfall">
-        <div class="sky-container">
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-        </div>
-    </section>
-  
-  <section class="mktitle">
-    <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="game.php">Game</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="admin.php">Admin</a></li>
-      </ul>
-    </nav>
-  </section> 
->>>>>>> 1ef43c6ecff0eb793dfbe1fdb4d48937aa9d5bf1
 </header>
 
 
