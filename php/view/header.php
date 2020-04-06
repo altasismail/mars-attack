b87bea49cb233d7d377a5f713a1f28bca5189a5d
merve16 <nmervekcygt@gmail.com>
2020-04-06 15:43:22 +0200

    m

--- php/view/header.php
+++ php/view/header.php
@@ -4,54 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="assets/css/style.css">
-=======
-    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
->>>>>>> 025b1195e83d8937c296ec104563e773e166429d
     <link rel="stylesheet" href="assets/css/altas.css">
     <link rel="stylesheet" href="assets/css/aydin.css">
     <link rel="stylesheet" href="assets/css/merve.css">
     <link rel="stylesheet" href="assets/css/neslihan.css">
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 025b1195e83d8937c296ec104563e773e166429d
 </head>
 <body>
   <header>
-  
-<<<<<<< HEAD
     <section class="mktitle">
-        <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-       <nav>
-         <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="blog.php">Blog</a></li>
-          <li><a href="game.php">Game</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <li><a href="admin.php">Admin</a></li>
-         </ul>
-       </nav>
-    </section> 
-
+       <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
+         <nav>
+           <ul>
+             <li><a href="index.php">Home</a></li>
+             <li><a href="blog.php">Blog</a></li>
+             <li><a href="game.php">Game</a></li>
+             <li><a href="contact.php">Contact</a></li>
+             <li><a href="admin.php">Admin</a></li>
+           </ul>
+         </nav>
+     </section> 
   </header>
-=======
-  <section class="mktitle">
-    <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="game.php">Game</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="admin.php">Admin</a></li>
-      </ul>
-    </nav>
-  </section> 
-</header>
->>>>>>> 025b1195e83d8937c296ec104563e773e166429d
-
 
   <main>
