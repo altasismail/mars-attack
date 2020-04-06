29c9322887b321fc44320f1a3ac97ca8bffcfb8f
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-06 14:08:06 +0200

    modified css and js links, and also header & footer pages.

---
+++ php/view/footer.php
@@ -0,0 +1,10 @@
+</main>
+    <footer>
+    
+    </footer>
+    <script src="assets/js/altas.js"></script>
+    <script src="assets/js/aydin.js"></script>
+    <script src="assets/js/merve.js"></script>
+    <script src="assets/js/neslihan.js"></script>
+</body>
+</html>
--- php/view/header.php
+++ php/view/header.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/altas.css">
+    <link rel="stylesheet" href="assets/css/aydin.css">
+    <link rel="stylesheet" href="assets/css/merve.css">
+    <link rel="stylesheet" href="assets/css/neslihan.css">
 
 </head>
 <body>
-<header>
-<section class="starfall">
+  <header>
+    <section class="starfall">
         <div class="sky-container">
           <div class="star"></div>
           <div class="star"></div>
@@ -17,24 +21,21 @@
           <div class="star"></div>
           <div class="star"></div>
         </div>
-  </section>
+    </section>
   
-  <section class="mervetitle">
-    <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="game.php">Game</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="admin.php">Admin</a></li>
-      </ul>
-    </nav>
-  </section> 
-</header>
+    <section class="mervetitle">
+      <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <li><a href="game.php">Game</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="admin.php">Admin</a></li>
+        </ul>
+      </nav>
+    </section> 
+  </header>
 
 
-
-    
-</body>
-</html>
+  <main>
