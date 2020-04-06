00cd65ecdc784d2de4798ed9d349daaf92d6978e
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-06 14:16:36 +0200

    deleted style css page

--- php/view/header.php
+++ php/view/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
     <link rel="stylesheet" href="assets/css/altas.css">
     <link rel="stylesheet" href="assets/css/aydin.css">
     <link rel="stylesheet" href="assets/css/merve.css">
@@ -23,21 +23,6 @@
         </div>
     </section>
   
-<<<<<<< HEAD
-    <section class="mervetitle">
-      <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="blog.php">Blog</a></li>
-          <li><a href="game.php">Game</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <li><a href="admin.php">Admin</a></li>
-        </ul>
-      </nav>
-    </section> 
-  </header>
-=======
   <section class="mktitle">
     <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
     <nav>
@@ -51,7 +36,6 @@
     </nav>
   </section> 
 </header>
->>>>>>> af8f928ca15c8833c62f55a5d9ae232d65e8ebe2
 
 
   <main>
