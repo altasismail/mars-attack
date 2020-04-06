025b1195e83d8937c296ec104563e773e166429d
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-06 15:37:41 +0200

    dd

--- php/view/header.php
+++ php/view/header.php
@@ -4,50 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="assets/css/style.css">
-=======
     <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
->>>>>>> 7863bada268582bf790374921836ca12337e2d14
     <link rel="stylesheet" href="assets/css/altas.css">
     <link rel="stylesheet" href="assets/css/aydin.css">
     <link rel="stylesheet" href="assets/css/merve.css">
     <link rel="stylesheet" href="assets/css/neslihan.css">
 
-<<<<<<< HEAD
-
-</head>
-<body>
-<header>
-  
-    <section class="mervetitle">
-        <section class="altas-logo">
-            <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-        </section>
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="blog.php">Blog</a></li>
-          <li><a href="game.php">Game</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <li><a href="admin.php">Admin</a></li>
-        </ul>
-      </nav>
-    </section> 
-  
-=======
 </head>
 <body>
   <header>
-    <section class="starfall">
-        <div class="sky-container">
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-        </div>
-    </section>
   
   <section class="mktitle">
     <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
@@ -61,7 +26,6 @@
       </ul>
     </nav>
   </section> 
->>>>>>> 7863bada268582bf790374921836ca12337e2d14
 </header>
 
 
