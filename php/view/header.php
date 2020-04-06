327c4af37dc52918207b424163a751d53a5d2158
altasismail <altasismail@gmail.com>
2020-04-06 14:33:05 +0200

    altas

--- php/view/header.php
+++ php/view/header.php
@@ -5,32 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/altas.css">
+    <link rel="stylesheet" href="assets/css/aydin.css">
+    <link rel="stylesheet" href="assets/css/merve.css">
+    <link rel="stylesheet" href="assets/css/neslihan.css">
+
 
 </head>
 <body>
 <header>
-<section class="starfall">
-        <div class="sky-container">
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-          <div class="star"></div>
-        </div>
-  </section>
   
-  <section class="mervetitle">
-    <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="game.php">Game</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="admin.php">Admin</a></li>
-      </ul>
-    </nav>
-  </section> 
+    <section class="mervetitle">
+      <section class="altas-logo">
+        <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
+      </section>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <li><a href="game.php">Game</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="admin.php">Admin</a></li>
+        </ul>
+      </nav>
+    </section> 
+  
 </header>
 
 
