de1b8c454b8942be27f1886b6f6e58543fda7893
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-08 15:53:36 +0200

    connection bdd et json

--- assets/js/aydin.js
+++ assets/js/aydin.js
@@ -1,1 +1,15 @@
 console.log("JS Aydin");
+
+
+// var buttonSignin = document.getElementById('buttonSignin');
+var buttonSignin = document.querySelector("#aydnLoginButton");
+
+buttonSignin.addEventListener("click",function getApiInfos(){
+    // Debug
+    // console.log("Click OK");
+    fetch("../../php/controller/apiFullInfo.php").then(function(response){
+        console.log(response.json());
+    });
+});
+
+
