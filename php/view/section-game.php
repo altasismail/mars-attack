034cad6e77118f930ccb2a1761530dfe86ccf36d
altasismail <altasismail@gmail.com>
2020-04-09 00:04:15 +0200

    board-fixed

--- php/view/section-game.php
+++ php/view/section-game.php
@@ -1,138 +1,106 @@
-<section class="altas-game">
-	<section class="altas-game-table" bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-		<table class="altas-game-table" id="Table_01" width="1200" height="1200" border="0" cellpadding="0" cellspacing="0">
-			<tr class="altas-game-row">
-				<td class="altas-game-table" colspan="13">
-					<img class="sides" src="assets/img/altas-game-images/altas-game_01.png" width="1200" height="62" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data" rowspan="12">
-					<img class="sides" src="assets/img/altas-game-images/altas-game_02.png" width="63" height="1138" alt=""></td>
-				<td>
-					<img class="free-space" id="free-space" src="assets/img/altas-game-images/altas-game_03.png" width="150" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="titan" id="titan" src="assets/img/altas-game-images/altas-game_04.png" width="85" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="new-discovery" id="new-discovery" src="assets/img/altas-game-images/altas-game_05.png" width="87" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="dione" id="dione" src="assets/img/altas-game-images/altas-game_06.png" width="86" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="saturn" id="saturn" src="assets/img/altas-game-images/altas-game_07.png" width="86" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="saturn-station" id="saturn-station" src="assets/img/altas-game-images/altas-game_08.png" width="86" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="ariel" id="ariel" src="assets/img/altas-game-images/altas-game_09.png" width="86" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="oberon" id="oberon" src="assets/img/altas-game-images/altas-game_10.png" width="87" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="interstellar-mining" id="interstellar-mining" src="assets/img/altas-game-images/altas-game_11.png" width="85" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="uranus" id="uranus" src="assets/img/altas-game-images/altas-game_12.png" width="86" height="151" alt=""></td>
-				<td class="altas-game-data">
-					<img class="blackhole" id="blackhole" src="assets/img/altas-game-images/altas-game_13.png" width="151" height="151" alt=""></td>
-				<td class="altas-game-data" rowspan="12">
-					<img class="sides" src="assets/img/altas-game-images/altas-game_14.png" width="62" height="1138" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="enceladus" id="enceladus" src="assets/img/altas-game-images/altas-game_15.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data" colspan="9" rowspan="9">
-					<img class="cards" id="cards" src="assets/img/altas-game-images/altas-game_16.png" width="774" height="776" alt=""></td>
-				<td class="altas-game-data">
-					<img class="triton" id="triton" src="assets/img/altas-game-images/altas-game_17.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="ganymede" id="ganymede" src="assets/img/altas-game-images/altas-game_18.png" width="150" height="87" alt=""></td>
-				<td class="altas-game-data">
-					<img class="neptune" id="neptune" src="assets/img/altas-game-images/altas-game_19.png" width="151" height="87" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="inventory-station" id="inventory-station" src="assets/img/altas-game-images/altas-game_20.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="inventory-station" id="inventory-station" src="assets/img/altas-game-images/altas-game_21.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="callisto" id="callisto" src="assets/img/altas-game-images/altas-game_22.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="pluto" id="pluto" src="assets/img/altas-game-images/altas-game_23.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="jupiter-station" id="jupiter-station" src="assets/img/altas-game-images/altas-game_24.png" width="150" height="87" alt=""></td>
-				<td class="altas-game-data">
-					<img class="pluto-station" id="pluto-station" src="assets/img/altas-game-images/altas-game_25.png" width="151" height="87" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="europa" id="europa" src="assets/img/altas-game-images/altas-game_26.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="new-discovery" id="new-discovery" src="assets/img/altas-game-images/altas-game_27.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="io" id="io" src="assets/img/altas-game-images/altas-game_28.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="alpha-centauri-a" id="alpha-centauri-a" src="assets/img/altas-game-images/altas-game_29.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="interstellar-network" id="interstellar-network" src="assets/img/altas-game-images/altas-game_30.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="anomaly-damage" id="anomaly-damage" src="assets/img/altas-game-images/altas-game_31.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="jupiter" id="jupiter" src="assets/img/altas-game-images/altas-game_32.png" width="150" height="86" alt=""></td>
-				<td class="altas-game-data">
-					<img class="alpha-centauri-b" id="alpha-centauri-b" src="assets/img/altas-game-images/altas-game_33.png" width="151" height="86" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data">
-					<img class="blackhole" id="blackhole" src="assets/img/altas-game-images/altas-game_34.png" width="150" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="mars" id="mars" src="assets/img/altas-game-images/altas-game_35.png" width="85" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="moon" id="moon" src="assets/img/altas-game-images/altas-game_36.png" width="87" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="new-discovery" id="new-discovery" src="assets/img/altas-game-images/altas-game_37.png" width="86" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="earth" id="earth" src="assets/img/altas-game-images/altas-game_38.png" width="86" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="earth-station" id="earth-station" src="assets/img/altas-game-images/altas-game_39.png" width="86" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="meteor-damage" id="meteor-damage" src="assets/img/altas-game-images/altas-game_40.png" width="86" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="venus" id="venus" src="assets/img/altas-game-images/altas-game_41.png" width="87" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="inventory-station" id="inventory-station" src="assets/img/altas-game-images/altas-game_42.png" width="85" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="mercure" id="mercure" src="assets/img/altas-game-images/altas-game_43.png" width="86" height="149" alt=""></td>
-				<td class="altas-game-data">
-					<img class="turn" id="turn" src="assets/img/altas-game-images/altas-game_44.png" width="151" height="149" alt=""></td>
-			</tr>
-			<tr class="altas-game-row">
-				<td class="altas-game-data" colspan="11">
-					<img class="altas-game-table" src="assets/img/altas-game-images/altas-game_45.png" width="1075" height="62" alt=""></td>
-			</tr>
-		</table>
-	</section> 
-	<section class="altas-table-info">
-		<table>
-			<tr>
-				<h2>Name:</h2>
-			</tr>
-			<tr><?php $name ?></tr>
-			<tr>
-				Cost:
-			</tr>
-			<tr><?php $cost ?></tr>
-			<tr>
-				<h3>Action: - / + O₂</h3>
-			</tr>
-
-		</table>
-	</section>
-</section>
+<table class="gameboard" id="gameboard">
+	<tr>
+		<td>
+			<img class="free-space" id="free-space" src="assets/img/altas-game-images/free-space.png" alt="free-space" /></td>
+		<td colspan="2">
+			<img class="titan" id="titan" src="assets/img/altas-game-images/titan.png" alt="titan" /></td>
+		<td>
+			<img class="new-discovery-1" id="new-discovery-1" src="assets/img/altas-game-images/new-discovery-1.png" alt="new-discovery-1" /></td>
+		<td>
+			<img class="dione" id="dione" src="assets/img/altas-game-images/dione.png" alt="dione" /></td>
+		<td>
+			<img class="saturn" id="saturn" src="assets/img/altas-game-images/saturn.png" alt="saturn" /></td>
+		<td>
+			<img class="saturn-station" id="saturn-station" src="assets/img/altas-game-images/saturn-station.png" alt="saturn-station" /></td>
+		<td>
+			<img class="ariel" id="ariel" src="assets/img/altas-game-images/ariel.png" alt="ariel" /></td>
+		<td>
+			<img class="oberon" id="oberon" src="assets/img/altas-game-images/oberon.png" alt="oberon" /></td>
+		<td>
+			<img class="interstelar-mining" id="interstelar-mining" src="assets/img/altas-game-images/interstelar-mining.png" alt="interstelar-mining" /></td>
+		<td>
+			<img class="uranus" id="uranus" src="assets/img/altas-game-images/uranus.png" alt="uranus" /></td>
+		<td>
+			<img class="blackhole" id="blackhole" src="assets/img/altas-game-images/blackhole.png" alt="blackhole" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="enceladus" id="enceladus" src="assets/img/altas-game-images/enceladus.png" alt="enceladus" /></td>
+		<td colspan="10" rowspan="9">
+			<img class="cards" id="cards" src="assets/img/altas-game-images/cards.png" alt="cards" /></td>
+		<td>
+			<img class="triton" id="triton" src="assets/img/altas-game-images/triton.png" alt="triton" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="ganymede" id="ganymede" src="assets/img/altas-game-images/ganymede.png" alt="ganymede" /></td>
+		<td>
+			<img class="neptune" id="neptune" src="assets/img/altas-game-images/neptune.png" alt="neptune" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="inventory-station-1" id="inventory-station-1" src="assets/img/altas-game-images/inventory-station-1.png" alt="inventory-station-1" /></td>
+		<td>
+			<img class="inventory-station-2" id="inventory-station-2" src="assets/img/altas-game-images/inventory-station-2.png" alt="inventory-station-2" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="callisto" id="callisto" src="assets/img/altas-game-images/callisto.png" alt="callisto" /></td>
+		<td>
+			<img class="pluto" id="pluto" src="assets/img/altas-game-images/pluto.png" alt="pluto" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="jupiter-station" id="jupiter-station" src="assets/img/altas-game-images/jupiter-station.png" alt="jupiter-station" /></td>
+		<td>
+			<img class="pluto-station" id="pluto-station" src="assets/img/altas-game-images/pluto-station.png" alt="pluto-station" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="europa" id="europa" src="assets/img/altas-game-images/europa.png" alt="europa" /></td>
+		<td>
+			<img class="new-discovery-2" id="new-discovery-2" src="assets/img/altas-game-images/new-discovery-2.png" alt="new-discovery-2" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="io" id="io" src="assets/img/altas-game-images/io.png" alt="io" /></td>
+		<td>
+			<img class="alpha-centauri-a" id="alpha-centauri-a" src="assets/img/altas-game-images/alpha-centauri-a.png" alt="alpha-centauri-a" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="interstellar-network" id="interstellar-network" src="assets/img/altas-game-images/interstellar-network.png" alt="interstellar-network" /></td>
+		<td>
+			<img class="anomaly-damage" id="anomaly-damage" src="assets/img/altas-game-images/anomaly-damage.png" alt="anomaly-damage" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="jupiter" id="jupiter" src="assets/img/altas-game-images/jupiter.png" alt="jupiter" /></td>
+		<td>
+			<img class="alpha-centauri-b" id="alpha-centauri-b" src="assets/img/altas-game-images/alpha-centauri-b.png" alt="alpha-centauri-b" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img class="just-escaped" id="just-escaped" src="assets/img/altas-game-images/just-escaped.png" alt="just-escaped" /></td>
+		<td>
+			<img class="mars" id="mars" src="assets/img/altas-game-images/mars.png" alt="mars" /></td>
+		<td colspan="2">
+			<img class="moon" id="moon" src="assets/img/altas-game-images/moon.png" alt="moon" /></td>
+		<td>
+			<img class="new-discovery-0" id="new-discovery-0" src="assets/img/altas-game-images/new-discovery-0.png" alt="new-discovery-0" /></td>
+		<td>
+			<img class="earth" id="earth" src="assets/img/altas-game-images/earth.png" alt="earth" /></td>
+		<td>
+			<img class="earth-station" id="earth-station" src="assets/img/altas-game-images/earth-station.png" alt="earth-station" /></td>
+		<td>
+			<img class="meteor-damage" id="meteor-damage" src="assets/img/altas-game-images/meteor-damage.png" alt="meteor-damage" /></td>
+		<td>
+			<img class="venus" id="venus" src="assets/img/altas-game-images/venus.png" alt="venus" /></td>
+		<td>
+			<img class="inventory-station-0" id="inventory-station-0" src="assets/img/altas-game-images/inventory-station-0.png" alt="inventory-station-0" /></td>
+		<td>
+			<img class="mercure" id="mercure" src="assets/img/altas-game-images/mercure.png" alt="mercure" /></td>
+		<td>
+			<img class="start" id="start" src="assets/img/altas-game-images/start.png" alt="start" /></td>
+	</tr>
+</table>
