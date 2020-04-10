629bc4f1296a8e385c82e2f20237ee38e650b73c
altasismail <altasismail@gmail.com>
2020-04-10 12:42:48 +0200

    touchable half

--- assets/js/altas.js
+++ assets/js/altas.js
@@ -1,4 +1,5 @@
-fetch("http://localhost/planetx/mars-attack/assets/data/ismailBoard.json")
+let gameBoardInfo = new Request ("http://localhost/planetx/mars-attack/assets/data/ismailBoard.json");
+fetch(gameBoardInfo)
     .then(function(response){
     return response.json();
 }).then(function(obj){
@@ -10,11 +11,64 @@ fetch("http://localhost/planetx/mars-attack/assets/data/ismailBoard.json")
 
 });
 
+//Messages functions=>
+
 function Start(){
-    
+    var startText = "<h2>Collect O2 200</h2>"
+    document.getElementById("property-info").innerHTML = startText;
+}
+
+function Mercure(){
+    var mercureText = "<h4>Welcome to Mercure, No atmosphere but plenty of cheap mines!</h4>"
+    document.getElementById("property-info").innerHTML = mercureText;
+}
+function Inventory_station(){
+    var inventoryText = "<h4>You have got a new inventory</h4>"
+    document.getElementById("property-info").innerHTML = inventoryText;
+}
+function Venus(){
+    var venusText = "<h4>Welcome to Venus, very thick atmosphere!</h4>"
+    document.getElementById("property-info").innerHTML = venusText;
+}
+function Meteor_damage(){
+    var meteorText = "<h4>You have lost your 200 of your Oxyjen containers!</h4>"
+    document.getElementById("property-info").innerHTML = meteorText;
+}
+function Earth_station(){
+    var earthStText = "<h4>Control all the stations to kneel the Empire!</h4>"
+    document.getElementById("property-info").innerHTML = earthStText;
+}
+function Earth(){
+    var earthText = "<h4>This was once your home!<br>Think agin if you have gone too far!<br>Never too late to turn back</h4>"
+    document.getElementById("property-info").innerHTML = earthText;
+}
+function New_discovery(){
+    var discoveryText = "<h4>Congratulation, You have made a new discovery!<br> Beware not all the discoveries are good!</h4>"
+    document.getElementById("property-info").innerHTML = discoveryText;
+}
+function Moon(){
+    var moonText = "<h4>You are a bit late to land on moon for the first time <br> but never too late to attack!</h4>"
+    document.getElementById("property-info").innerHTML = moonText;
+}
+function Mars(){
+    var marsText = "<h4>On Mars act like a martian! Attack!</h4>"
+    document.getElementById("property-info").innerHTML = marsText;
+}
+function Blackhole(){
+    var blackholeText = "<h4>Roll all your engines, make sure they both in full speed!</h4>"
+    document.getElementById("property-info").innerHTML = blackholeText;
+}
+function Jupiter(){
+    var jupiterText = "<h4>Bevare of the gravity! Jupiter has you now!</h4>"
+    document.getElementById("property-info").innerHTML = jupiterText;
+}
+function Interstellar_network(){
+    var networkText = "<h4>Control all the communication..  So you got the power!</h4>"
+    document.getElementById("property-info").innerHTML = networkText;
 }
 function FreeSpace(){
-    alert("Bonne Voyage Space Traveler!")
+    var freeSpaceText = "<h2>Bonne Voyage Space Advanturer</h2>"
+    document.getElementById("property-info").innerHTML = freeSpaceText;
 }
 // let turn = document.getElementById($id=function getid(start){
 
--- php/view/section-game.php
+++ php/view/section-game.php
@@ -3,114 +3,109 @@
 		<tr>
 			<td onclick="FreeSpace()">
 				<img class="free-space" id="free-space" src="assets/img/altas-game-images/free-space.png" alt="free-space" /></td>
-			<td colspan="2">
+			<td colspan="2" onclick="Titan()">
 				<img class="titan" id="titan" src="assets/img/altas-game-images/titan.png" alt="titan" /></td>
-			<td>
+			<td onclick="New_discovery()">
 				<img class="new-discovery-1" id="new-discovery-1" src="assets/img/altas-game-images/new-discovery-1.png" alt="new-discovery-1" /></td>
-			<td>
+			<td onclick="Dione()">
 				<img class="dione" id="dione" src="assets/img/altas-game-images/dione.png" alt="dione" /></td>
-			<td>
+			<td onclick="Saturn()">
 				<img class="saturn" id="saturn" src="assets/img/altas-game-images/saturn.png" alt="saturn" /></td>
-			<td>
+			<td onclick="Saturn_station()">
 				<img class="saturn-station" id="saturn-station" src="assets/img/altas-game-images/saturn-station.png" alt="saturn-station" /></td>
-			<td>
+			<td onclick="Ariel()">
 				<img class="ariel" id="ariel" src="assets/img/altas-game-images/ariel.png" alt="ariel" /></td>
-			<td>
+			<td onclick="Oberon()">
 				<img class="oberon" id="oberon" src="assets/img/altas-game-images/oberon.png" alt="oberon" /></td>
-			<td>
+			<td onclick="Interstellar_mining()">
 				<img class="interstelar-mining" id="interstelar-mining" src="assets/img/altas-game-images/interstelar-mining.png" alt="interstelar-mining" /></td>
-			<td>
+			<td onclick="Uranus()">
 				<img class="uranus" id="uranus" src="assets/img/altas-game-images/uranus.png" alt="uranus" /></td>
-			<td>
+			<td onclick="Blackhole()">
 				<img class="blackhole" id="blackhole" src="assets/img/altas-game-images/blackhole.png" alt="blackhole" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Enceladus()">
 				<img class="enceladus" id="enceladus" src="assets/img/altas-game-images/enceladus.png" alt="enceladus" /></td>
 			<td colspan="10" rowspan="9">
 				<img class="cards" id="cards" src="assets/img/altas-game-images/cards.png" alt="cards" /></td>
-			<td>
+			<td onclick="Triton()">
 				<img class="triton" id="triton" src="assets/img/altas-game-images/triton.png" alt="triton" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Ganymede()">
 				<img class="ganymede" id="ganymede" src="assets/img/altas-game-images/ganymede.png" alt="ganymede" /></td>
-			<td>
+			<td onclick="Neptune()">
 				<img class="neptune" id="neptune" src="assets/img/altas-game-images/neptune.png" alt="neptune" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Inventory_station()">
 				<img class="inventory-station-1" id="inventory-station-1" src="assets/img/altas-game-images/inventory-station-1.png" alt="inventory-station-1" /></td>
-			<td>
+			<td onclick="Inventory_station()">
 				<img class="inventory-station-2" id="inventory-station-2" src="assets/img/altas-game-images/inventory-station-2.png" alt="inventory-station-2" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Callisto()">
 				<img class="callisto" id="callisto" src="assets/img/altas-game-images/callisto.png" alt="callisto" /></td>
-			<td>
+			<td onclick="Pluto()">
 				<img class="pluto" id="pluto" src="assets/img/altas-game-images/pluto.png" alt="pluto" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Jupiter_station()">
 				<img class="jupiter-station" id="jupiter-station" src="assets/img/altas-game-images/jupiter-station.png" alt="jupiter-station" /></td>
-			<td>
+			<td onclick="Pluto_station()">
 				<img class="pluto-station" id="pluto-station" src="assets/img/altas-game-images/pluto-station.png" alt="pluto-station" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Europa()">
 				<img class="europa" id="europa" src="assets/img/altas-game-images/europa.png" alt="europa" /></td>
-			<td>
+			<td onclick="New_discovery()">
 				<img class="new-discovery-2" id="new-discovery-2" src="assets/img/altas-game-images/new-discovery-2.png" alt="new-discovery-2" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Io()">
 				<img class="io" id="io" src="assets/img/altas-game-images/io.png" alt="io" /></td>
-			<td>
+			<td onclick="Alpha_centauri_a()">
 				<img class="alpha-centauri-a" id="alpha-centauri-a" src="assets/img/altas-game-images/alpha-centauri-a.png" alt="alpha-centauri-a" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Interstellar_network()">
 				<img class="interstellar-network" id="interstellar-network" src="assets/img/altas-game-images/interstellar-network.png" alt="interstellar-network" /></td>
-			<td>
+			<td onclick="Anomaly_damage()">
 				<img class="anomaly-damage" id="anomaly-damage" src="assets/img/altas-game-images/anomaly-damage.png" alt="anomaly-damage" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Jupiter()">
 				<img class="jupiter" id="jupiter" src="assets/img/altas-game-images/jupiter.png" alt="jupiter" /></td>
-			<td>
+			<td onclick="Alpha_centauri_b()">
 				<img class="alpha-centauri-b" id="alpha-centauri-b" src="assets/img/altas-game-images/alpha-centauri-b.png" alt="alpha-centauri-b" /></td>
 		</tr>
 		<tr>
-			<td>
+			<td onclick="Just_escaped()">
 				<img class="just-escaped" id="just-escaped" src="assets/img/altas-game-images/just-escaped.png" alt="just-escaped" /></td>
-			<td>
+			<td onclick="Mars()">
 				<img class="mars" id="mars" src="assets/img/altas-game-images/mars.png" alt="mars" /></td>
-			<td colspan="2">
+			<td colspan="2" onclick="Moon()">
 				<img class="moon" id="moon" src="assets/img/altas-game-images/moon.png" alt="moon" /></td>
-			<td>
+			<td onclick="New_discovery()">
 				<img class="new-discovery-0" id="new-discovery-0" src="assets/img/altas-game-images/new-discovery-0.png" alt="new-discovery-0" /></td>
-			<td>
+			<td onclick="Earth()">
 				<img class="earth" id="earth" src="assets/img/altas-game-images/earth.png" alt="earth" /></td>
-			<td>
+			<td onclick="Earth_station()">
 				<img class="earth-station" id="earth-station" src="assets/img/altas-game-images/earth-station.png" alt="earth-station" /></td>
-			<td>
+			<td onclick="Meteor_damage()">
 				<img class="meteor-damage" id="meteor-damage" src="assets/img/altas-game-images/meteor-damage.png" alt="meteor-damage" /></td>
-			<td>
+			<td onclick="Venus()">
 				<img class="venus" id="venus" src="assets/img/altas-game-images/venus.png" alt="venus" /></td>
-			<td>
+			<td onclick="Inventory_station()">
 				<img class="inventory-station-0" id="inventory-station-0" src="assets/img/altas-game-images/inventory-station-0.png" alt="inventory-station-0" /></td>
-			<td>
+			<td onclick="Mercure()">
 				<img class="mercure" id="mercure" src="assets/img/altas-game-images/mercure.png" alt="mercure" /></td>
 			<td onclick="Start()">
 				<img class="start" id="start" src="assets/img/altas-game-images/start.png" alt="start" /></td>
 		</tr>
 	</table>
 	<section class="altas-gameside">
-		<div id="message"><h1>MARS ATTACK</h1></div>
-
-		<div id="player-info"></div>
-		<div id="property-info"></div>
-		
 		<div class="dice" id="dice">
 			<div class="die one" id="die-1"><span class="dot"></span></div>
 			<div class="die one" id="die-2"><span class="dot"></span></div>
@@ -125,6 +120,12 @@
 			<input type="button" value="Unmortgage" id="btn-unmortgage"><br />
 			<input type="button" value="Trade" id="btn-trade">
 		</div>
+	</section>
+	<section class="ismail-change-info">
+		<div id="message"><h1>MARS ATTACK</h1></div>
+		<div id="player-info"></div>
+		<div id="property-info"><h2>Start</h2></div>
+
 	</section>
 
 </section>
