d621ed32f33af5f3bdae18e70774bb16358ae6a2
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-10 11:36:32 +0200

    admin blog page

--- php/view/section-adminblog.php
+++ php/view/section-adminblog.php
@@ -10,3 +10,24 @@
     </form>
 </div>
 </section>
+
+
+
+<h2 class="aydnBlogListTitle">All Players List & Informations</h2>
+
+<table class="aydnAllBlogsList">
+  <tr class="aydnBlogTitleColones">
+    <th class="aydnBlogColoneId">Id</th>
+    <th class="aydnBlogColoneName">Image</th>
+    <th class="aydnBlogColoneSurname">Title</th>
+    <th class="aydnBlogColoneEmail">Text</th>
+  </tr>
+  <tr calss="aydnOneBlogInfos">
+    <td class="aydnBlogId">1</td>
+    <td class="aydnBlogName"><img src="assets/img/blogPhotos/marsian.jpg" alt=""></td>
+    <td class="aydnBlogSurname">Marsian a dit que Salut à tout l'univers</td>
+    <td class="aydnBlogEmail"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident dolores sapiente quisquam beatae optio, impedit ipsa a dolorem, tenetur magni doloribus mollitia dicta culpa temporibus pariatur, fugit neque ad voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti, voluptates veritatis iure quasi vero aliquid atque omnis quibusdam cum voluptatum laborum tempore recusandae ad neque beatae laboriosam cumque repellendus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eaque ex vero numquam reiciendis perferendis, voluptas, consequuntur, corrupti aspernatur nobis quae cumque. Voluptate itaque quidem, porro corrupti natus totam dolor. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quaerat, unde necessitatibus soluta facilis exercitationem illum quibusdam, saepe reiciendis modi in atque ipsam! Itaque quidem voluptatum, aut ipsum officia quibusdam!
+    </p></td>
+  </tr>
+  
+</table>
