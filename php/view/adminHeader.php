283934c09a40d392bceabeb1191a0a01f4c204f6
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-10 11:13:03 +0200

    admin header

---
+++ php/view/adminHeader.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/altas.css">
+    <link rel="stylesheet" href="assets/css/aydin.css">
+    <link rel="stylesheet" href="assets/css/merve.css">
+    <link rel="stylesheet" href="assets/css/neslihan.css">
+    <link rel="stylesheet" href="assets/css/general.css">
+
+</head>
+<body>
+  <header>
+    <section class="mkadmin2title">
+       <img src="assets/img/logo-min.png" href="index.php" alt="Logo Mars Attack">
+         <nav class="mkadmin2nav">
+           <ul>
+             <li><a class="mkadmin2"   href="admin2.php">Personal Information</a></li>
+             <li><a class="mkadminblog"    href="adminBlog.php">Blog List & New Blog</a></li>
+             <li><a href="adminPlayersList.php">Players List</a></li>
+           </ul>
+         </nav>
+     </section> 
+  </header>
+
+  <main>
