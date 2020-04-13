ec634ff0dce802404ed70c3dcb87b246d75b21ad
Ismail Aydin <i.aydin20014@gmail.com>
2020-04-14 01:19:50 +0200

    modified admin players list

---
+++ php/view/section-adminPlayersList.php
@@ -0,0 +1,21 @@
+<body onload="aydnAllPlayersList()">
+<h2 class="aydnPlayerListTitle">All Players List & Informations</h2>
+
+<table class="aydnAllPlayersList">
+  <!-- I used them (html codes in comment lines) just for edit css, now they come from 'aydin.js' -->
+  <!-- <tr class="aydnTitleColones">
+    <th class="aydnColoneId">Id</th>
+    <th class="aydnColoneName">Name</th>
+    <th class="aydnColoneSurname">Surname</th>
+    <th class="aydnColoneEmail">Email</th>
+    <th class="aydnColonePassword">Password</th>
+  </tr>
+  <tr calss="aydnOnePlayerInfos">
+    <td class="aydnPlayerId">1</td>
+    <td class="aydnPlayerName">Maria</td>
+    <td class="aydnPlayerSurname">Anders</td>
+    <td class="aydnPlayerEmail">maria@mail.com</td>
+    <td class="aydnPlayerPassword">123456</td>
+  </tr> -->
+  
+</table>
